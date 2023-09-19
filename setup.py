400df6c99c4a907bd42849d39fcb950a121fc9d8
Andrés Granados <afgranadosc@unal.edu.co>
2023-09-19 16:33:38 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,10 +4,15 @@ from setuptools import setup, find_packages
 setup(
     name="similarity",
     version="0.1.2",
-    description="A collection of NLP tools for text-preprocessing tasks.",
+    description="Compute the similarity between two words or sentences.",
     packages=find_packages(),
     install_requires=[
         d for d in open("requirements.txt").readlines() if not d.startswith("--")
     ],
     package_dir={"": "."},
+    entry_points={
+        "console_scripts": [
+            "similarity = main:main",
+        ]
+    },
 )
